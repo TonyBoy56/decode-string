6ec593debfae89cabf5a1f1ba8fbb29cd57b5eaf
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-04-20 14:04:44 -0700

    add code to decode a string using stacks, check 'res' for contents after parseInt and .repeat method.

---
+++ decode.js
@@ -0,0 +1,66 @@
+// From Leetcode: Given an encoded string, return its decoded string.
+
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+// You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+// The test cases are generated so that the length of the output will never exceed 105.
+
+// Example 1: Input: s = "3[a]2[bc]"
+// Output: "aaabcbc"
+
+// Example 2: Input: s = "3[a2[c]]"
+// Output: "accaccacc"
+
+// Example 3: Input: s = "2[abc]3[cd]ef"
+// Output: "abcabccdcdcdef"
+
+// let numStack = [];
+// let stringStack = [];
+
+const stringToDecode = "3[a]2[bc]"
+
+function decodeString(stringToDecode) {
+    let i = 0;
+    const stack = [];
+
+    while (i < stringToDecode.length) {
+        if (stringToDecode[i] !== ']') {
+            stack.push(stringToDecode[i]);
+        } else {
+            let [res, rep, top] = ['', '', ''];
+            
+            while (stack.length) {
+                top = stack.pop();
+                if (top === '[') {
+                    break;
+                } else {
+                    res = top + res
+                }
+            }
+
+            while(stack.length) {
+                top = stack.pop();
+                if(top >= '0' && top <= '9') {
+                    rep = top + rep;
+                } else {
+                    stack.push(top);
+                    break;
+                }
+            }
+
+            res = res.repeat(Number.parseInt(rep));
+            console.log(res);
+            stack.push(res);
+        }
+        i++ 
+    }
+
+    res = '';
+    while (stack.length) {
+        res = stack.pop() + res;
+    }
+    // console.log(res);
+}
+
+decodeString(stringToDecode);
